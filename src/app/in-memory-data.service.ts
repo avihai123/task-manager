b77cf7890f05a174e2fe22d108c15009c4381759
avihai <avihai.yosef@getwizer.com>
2018-10-31 14:33:53 +0200

    add dummy data

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,19 +3,47 @@ import {Task} from './tasks/models/task.model';
 
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
-    const tasks = [
-      {
-        id: 1,
-        parentId: null,
-        title: 'Build task manager',
-        description: 'build task manager',
-        statusId: 1,
-        subTaskIds: [],
-      },
-    ];
+    // const tasks = [
+    //   {
+    //     id: 1,
+    //     parentId: null,
+    //     title: 'Build task manager',
+    //     description: 'build task manager',
+    //     statusId: 1,
+    //     subTaskIds: [],
+    //   },
+    // ];
+    const tasks = generateDummy();
     return {tasks};
   }
   genId(tasks: Task[]): number {
     return tasks.length > 0 ? Math.max(...tasks.map(task => task.id)) + 1 : 1;
   }
+}
+
+function generateDummy(){
+  const n = 5;
+  const tasks = [];
+  for (let i = 1; i <= n; i++) {
+    const subTaskIds = [];
+    for (let j = 1; j <= 3; j++) {
+      const task = {
+        id: n + i + j,
+        parentId: i,
+        title: `Node ${n + i + j}`,
+        statusId: 2,
+        subTaskIds: [],
+      };
+      tasks.push(task);
+      subTaskIds.push(task.id);
+    }
+    tasks.push({
+      id: i,
+      parentId: null,
+      title: `Node ${i}`,
+      statusId: 2,
+      subTaskIds: subTaskIds,
+    });
+  }
+  return tasks;
 }
