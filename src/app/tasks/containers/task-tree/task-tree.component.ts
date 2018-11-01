5f742e5147a18f168544727a036b0aef15b27f50
avihai <avihai.yosef@getwizer.com>
2018-11-01 12:15:20 +0200

    add loading div + add back to tasks button

--- src/app/tasks/containers/task-tree/task-tree.component.ts
+++ src/app/tasks/containers/task-tree/task-tree.component.ts
@@ -12,11 +12,13 @@ import * as fromStore from '../../store';
 })
 export class TaskTreeComponent implements OnInit {
   taskRootIds$: Observable<number[]>;
+  isLoading$: Observable<boolean>;
 
   constructor(private store: Store<fromStore.TasksState>) {
   }
 
   ngOnInit() {
     this.taskRootIds$ = this.store.select(fromStore.getRootTaskIds);
+    this.isLoading$ = this.store.select(fromStore.getTasksLoading);
   }
 }
