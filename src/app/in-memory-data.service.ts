e11f571c3d303554fdeacd92e5c97722a8fdbca1
avihai <avihai.yosef@getwizer.com>
2018-11-01 13:58:18 +0200

    refactor

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -3,47 +3,32 @@ import {Task} from './tasks/models/task.model';
 
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
-    // const tasks = [
-    //   {
-    //     id: 1,
-    //     parentId: null,
-    //     title: 'Build task manager',
-    //     description: 'build task manager',
-    //     statusId: 1,
-    //     subTaskIds: [],
-    //   },
-    // ];
     const tasks = generateTree();
     return {tasks};
   }
+
   genId(tasks: Task[]): number {
     return tasks.length > 0 ? Math.max(...tasks.map(task => task.id)) + 1 : 1;
   }
 }
 
 function generateTree() {
-  const n = 5;
-  const tasks = [];
-  for (let i = 1; i <= n; i++) {
-    const subTaskIds = [];
-    for (let j = 1; j <= 3; j++) {
-      const task = {
-        id: n + i + j,
-        parentId: i,
-        title: `Node ${n + i + j}`,
-        statusId: 2,
-        subTaskIds: [],
-      };
-      tasks.push(task);
-      subTaskIds.push(task.id);
+  const tree = [];
+  for (let i = 0; i < 25; i++) {
+    let parentId = Math.floor(Math.pow(Math.random(), 2) * i);
+
+    if (parentId === 0) {
+      parentId = null;
+    } else {
+      tree[parentId].subTaskIds.push(i);
     }
-    tasks.push({
+    tree.push({
+      parentId: parentId,
       id: i,
-      parentId: null,
-      title: `Node ${i}`,
-      statusId: 2,
-      subTaskIds: subTaskIds,
+      title: `Task ${i}`,
+      statusId: 1,
+      subTaskIds: []
     });
   }
-  return tasks;
+  return tree;
 }
